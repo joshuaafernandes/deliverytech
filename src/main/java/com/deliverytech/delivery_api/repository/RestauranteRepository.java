bd8813cf4a0f350d25fd01325c7081d4ac7f8a9a
joshuaafernandes <joshssoftware@gmail.com>
2026-01-23 21:25:15 -0300

    Adicionando os repositories

---
+++ src/main/java/com/deliverytech/delivery_api/repository/RestauranteRepository.java
@@ -0,0 +1,25 @@
+package com.deliverytech.delivery_api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.deliverytech.delivery_api.model.Restaurante;
+
+
+@Repository
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+        List<Restaurante> findByCategoria(String categoria);
+
+        List<Restaurante> findByAtivoTrue();
+
+        List<Restaurante> findByAtivoTrueOrderByAvaliacaoDesc();
+
+        List<Restaurante> findByNomeContainingIgnoreCase(String nome);
+
+        List<Restaurante> findByCategoriaAndAtivoTrue(String categoria);
+
+        
+
+}
