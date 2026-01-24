c018cf4a09ec8650f68b7a849a74f424c863c14f
joshuaafernandes <joshssoftware@gmail.com>
2026-01-23 21:23:51 -0300

    Adicionando as entities e enum.

---
+++ src/main/java/com/deliverytech/delivery_api/model/Cliente.java
@@ -0,0 +1,62 @@
+package com.deliverytech.delivery_api.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome; 
+
+    @Column(unique= true, nullable = false)
+    private String email;
+
+    private String telefone;
+
+    private String endereco; 
+
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    private boolean ativo;  
+
+    @OneToMany(mappedBy="cliente")
+    @JsonIgnore
+    private List<Pedido> pedidos = new ArrayList<>();
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
